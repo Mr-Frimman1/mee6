19e339e12e19fd5ce296c69fb04f1f0c5d8abc28
Mr-Frimman1 <84664029+Mr-Frimman1@users.noreply.github.com>
2021-06-07 14:34:44 +0200

    Update exports.sh

--- exports.sh
+++ exports.sh
@@ -3,9 +3,9 @@
 # This is a set of commands that need to be appended to
 # ~/.bashrc in order for the bot to work. See the Readme for more info.
 export REDIS_URL=redis://localhost
-export OAUTH2_CLIENT_ID=#Discord client ID for your bot
-export OAUTH2_CLIENT_SECRET=#your Discord bot's client secret
-export MEE6_TOKEN=#Your Discord bot token
+export OAUTH2_CLIENT_ID=840581574918930473 #Discord client ID for your bot
+export OAUTH2_CLIENT_SECRET=AH8Glb0xvAKkmivIHVlE0CI7_XHH8k-J #your Discord bot's client secret
+export MEE6_TOKEN=ODQwNTgxNTc0OTE4OTMwNDcz.YJaSmA.i884oBEN8beQpuHkFf_ES7gbymo #Your Discord bot token
 export OAUTH2_REDIRECT_URI=http://localhost:5000/confirm_login #change to your domain if needed
 export MAL_USERNAME=#username for account on http://myanimelist.net
 export MAL_PASSWORD=#password for above site
